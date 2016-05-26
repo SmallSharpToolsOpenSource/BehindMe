310cc05ce46dedfab3b2fa8ba9f56b8e14df310d
brennanMKE <brennan@smallsharptools.com>
2016-05-26 16:35:32 -0700

    positioned background screenshot accurately

--- BehindMe/CollectionViewController.m
+++ BehindMe/CollectionViewController.m
@@ -10,44 +10,88 @@
 
 @interface CollectionViewController ()
 
+@property (nonatomic) BOOL didFirstToggle;
+@property (nonatomic) BOOL showBackgroundImage;
+
+@property (readonly, nonatomic) UIImage *screenshotImage;
+
 @end
 
 @implementation CollectionViewController
 
-static NSString * const reuseIdentifier = @"Cell";
-
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
 
-//    UIImage *image = [UIImage imageNamed:@"CaptainAmerica.jpg"];
-//    self.collectionView.backgroundColor = [UIColor colorWithPatternImage:image];
+- (void)viewDidLayoutSubviews {
+    if (!self.didFirstToggle) {
+        self.didFirstToggle = YES;
+        [self toggleBackground];
+    }
+}
 
-//    UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.collectionView.bounds];
-//    imageView.contentMode = UIViewContentModeScaleAspectFill;
-//    imageView.image = image;
-//    self.collectionView.backgroundView = imageView;
+- (void)toggleBackground {
+    CGRect frame = self.collectionView.frame;
+    UIEdgeInsets inset = self.collectionView.contentInset;
+    frame.origin.y = inset.top;
 
-    UIImage *image = [UIImage imageNamed:@"CaptainAmerica.jpg"];
-    UIView *backgroundView = [[UIView alloc] initWithFrame:self.collectionView.bounds];
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.collectionView.bounds];
-    imageView.contentMode = UIViewContentModeScaleAspectFill;
-    imageView.image = image;
-    UIView *overlayView = [[UIView alloc] initWithFrame:self.collectionView.bounds];
-    overlayView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.75];
-    [backgroundView addSubview:imageView];
-    [backgroundView addSubview:overlayView];
+    self.showBackgroundImage = !self.showBackgroundImage;
+
+    UIView *backgroundView = nil;
+
+    if (self.showBackgroundImage) {
+        backgroundView = [[UIView alloc] initWithFrame:frame];
+        UIImageView *screenshotImageView = [[UIImageView alloc] initWithFrame:frame];
+        screenshotImageView.image = self.screenshotImage;
+        screenshotImageView.contentMode = UIViewContentModeScaleAspectFit;
+        screenshotImageView.alpha = 0.5;
+        UIImageView *captainImageView = [[UIImageView alloc] initWithFrame:frame];
+        captainImageView.image = [UIImage imageNamed:@"CaptainAmerica.jpg"];
+        captainImageView.contentMode = UIViewContentModeScaleAspectFill;
+        UIView *overlayView = [[UIView alloc] initWithFrame:frame];
+        overlayView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.75];
+        [backgroundView addSubview:captainImageView];
+        [backgroundView addSubview:screenshotImageView];
+        [backgroundView addSubview:overlayView];
+    }
+    else {
+        backgroundView = [[UIView alloc] initWithFrame:frame];
+        backgroundView.backgroundColor = [UIColor blackColor];
+    }
+
+    self.collectionView.backgroundColor = [UIColor blackColor];
     self.collectionView.backgroundView = backgroundView;
+    [self.collectionView reloadData];
+}
+
+- (UIImage *)screenshotImage {
+    return [self imageWithView:self.collectionView afterScreenUpdate:YES];
+}
+
+- (UIImage *)imageWithView:(UIView *)view afterScreenUpdate:(BOOL)afterScreenUpdate {
+    UIGraphicsBeginImageContextWithOptions(view.bounds.size, view.opaque, 0.0);
+    [view drawViewHierarchyInRect:view.bounds afterScreenUpdates:afterScreenUpdate];
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return img;
+}
+
+- (IBAction)toggleButtonTapped:(id)sender {
+    [self toggleBackground];
 }
 
 #pragma mark <UICollectionViewDataSource>
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return 100;
+    return self.showBackgroundImage ? 10: 100;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"BasicCell" forIndexPath:indexPath];
-    
+
+    UILabel *label = (UILabel *)[cell viewWithTag:1];
+    label.text = [NSString stringWithFormat:@"%ld", (long)(indexPath.row + 1)];
+
     return cell;
 }
 
--- BehindMe/TableViewController.m
+++ BehindMe/TableViewController.m
@@ -10,34 +10,89 @@
 
 @interface TableViewController ()
 
+@property (nonatomic) BOOL didFirstToggle;
+@property (nonatomic) BOOL showBackgroundImage;
+
+@property (readonly, nonatomic) UIImage *screenshotImage;
+
 @end
 
 @implementation TableViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+}
 
-    UIImage *image = [UIImage imageNamed:@"CaptainAmerica.jpg"];
-    UIView *backgroundView = [[UIView alloc] initWithFrame:self.tableView.bounds];
-    UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.tableView.bounds];
-    imageView.contentMode = UIViewContentModeScaleAspectFill;
-    imageView.image = image;
-    UIView *overlayView = [[UIView alloc] initWithFrame:self.tableView.bounds];
-    overlayView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.75];
-    [backgroundView addSubview:imageView];
-    [backgroundView addSubview:overlayView];
+- (void)viewDidLayoutSubviews {
+    if (!self.didFirstToggle) {
+        self.didFirstToggle = YES;
+        [self toggleBackground];
+    }
+}
+
+- (void)toggleBackground {
+    CGRect frame = self.tableView.frame;
+    UIEdgeInsets inset = self.tableView.contentInset;
+    frame.origin.y = inset.top;
+
+    self.showBackgroundImage = !self.showBackgroundImage;
+
+    UIView *backgroundView = nil;
+
+    if (self.showBackgroundImage) {
+        backgroundView = [[UIView alloc] initWithFrame:frame];
+        UIImageView *screenshotImageView = [[UIImageView alloc] initWithFrame:frame];
+        screenshotImageView.image = self.screenshotImage;
+        screenshotImageView.contentMode = UIViewContentModeScaleAspectFit;
+        screenshotImageView.alpha = 0.5;
+        UIImageView *captainImageView = [[UIImageView alloc] initWithFrame:frame];
+        captainImageView.image = [UIImage imageNamed:@"CaptainAmerica.jpg"];
+        captainImageView.contentMode = UIViewContentModeScaleAspectFill;
+        UIView *overlayView = [[UIView alloc] initWithFrame:frame];
+        overlayView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.75];
+        [backgroundView addSubview:captainImageView];
+        [backgroundView addSubview:screenshotImageView];
+        [backgroundView addSubview:overlayView];
+    }
+    else {
+        backgroundView = [[UIView alloc] initWithFrame:frame];
+        backgroundView.backgroundColor = [UIColor blackColor];
+    }
+
+    self.tableView.backgroundColor = [UIColor blackColor];
     self.tableView.backgroundView = backgroundView;
+    [self.tableView reloadData];
+}
+
+- (IBAction)toggleButtonTapped:(id)sender {
+    [self toggleBackground];
+}
+
+- (UIImage *)screenshotImage {
+    return [self imageWithView:self.tableView afterScreenUpdate:YES];
+}
+
+- (UIImage *)imageWithView:(UIView *)view afterScreenUpdate:(BOOL)afterScreenUpdate {
+    UIGraphicsBeginImageContextWithOptions(view.bounds.size, view.opaque, 0.0);
+    [view drawViewHierarchyInRect:view.bounds afterScreenUpdates:afterScreenUpdate];
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return img;
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+    return self.showBackgroundImage ? 5: 100;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"BasicCell" forIndexPath:indexPath];
     cell.textLabel.text = [NSString stringWithFormat:@"Row %ld", (long)(indexPath.row + 1)];
+
+    UIView *selectedBackgroundView = [[UIView alloc] initWithFrame:cell.bounds];
+    selectedBackgroundView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.25];
+    cell.selectedBackgroundView = selectedBackgroundView;
 
     return cell;
 }
